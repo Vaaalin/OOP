f98b7337292e602f92dbae0771628aa171992c11
Alvin Kyalo <kyaloalviin@gmail.com>
2025-04-29 13:37:05 +0300

    Add three dice rolls with bonus points for doubles/triples and win condition

--- dice.java
+++ dice.java
@@ -3,7 +3,27 @@ import java.util.Random;
 public class dice {
     public static void main(String[] args) {
         Random random = new Random();
-        int roll = random.nextInt(6) + 1;
-        System.out.println("You rolled a " + roll);
+        int roll1 = random.nextInt(6) + 1;
+        int roll2 = random.nextInt(6) + 1;
+        int roll3 = random.nextInt(6) + 1;
+        int total = roll1 + roll2 + roll3;
+
+        System.out.println("Dice roll: " + roll1 + " + " + roll2 + " + " + roll3);
+
+        if ((roll1 == roll2) && (roll2 == roll3)) {
+            System.out.println("You rolled tripples! +6 bonus to total!");
+            total += 6;
+        } else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+            System.out.println("You rolled doubles! +2 bonus to total!");
+            total += 2;
+        }
+
+        System.out.println("Total score: " + total);
+
+        if (total >= 15) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("Sorry, you lose.");
+        }
     }
 }
